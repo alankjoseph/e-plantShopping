148094e0ffc0863e6268c89e54afdc699772a1c8
alankjoseph <alanjosephclt@gmail.com>
2025-03-01 08:51:03 -0500

    integrated redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        console.log("inside addItem")
+        const {name, image, cost} = action.payload
+        const existingItem = state.items.find(item => item.name === name)
+        if(existingItem){
+            existingItem.quantity++
+        }else{
+            state.items.push({name,image,cost,quantity:1})
+            console.log('item is added')
+        }
     
     },
     removeItem: (state, action) => {
+        const {name} = action.payload
+        const itemToRemove = state.items.find(item => item.name === name)
+        if(itemToRemove){
+            state.items = state.items.filter(item=> item.name!==name)
+        }
     },
     updateQuantity: (state, action) => {
-
+        const {name,quantity} = action.payload
+        const itemToUpdate = state.items.find(item => item.name === name)
+        if(itemToUpdate){
+            itemToUpdate.quantity = quantity
+        }
     
     },
   },
